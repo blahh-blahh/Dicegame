87dff4c7ae9ed65162c202c51fe8825ff39750a9
blahh-blahh <85347713+blahh-blahh@users.noreply.github.com>
2023-03-04 18:08:25 +0530

    Update dicee.js

--- dicee.js
+++ dicee.js
@@ -1,6 +1,7 @@
 var state="initial";
-state=prompt("Wanna play. Write YO if yes");
-if(state=="YO"){
+state=prompt("Select to be Player 1 or 2.");
+if(state=="initial"){}
+else{
 var randomVar1=Math.floor(Math.random(3)*6)+1;
 var randomVar2=Math.floor(Math.random(3)*6)+1;
 var randomDiceImg1="images/dice"+randomVar1+".png";
@@ -8,7 +9,7 @@ document.querySelector(".img1").setAttribute("src",randomDiceImg1);
 var randomDiceImg2="images/dice"+randomVar2+".png";
 document.querySelectorAll("img")[1].setAttribute("src",randomDiceImg2);
 if(randomVar1>randomVar2)
-document.querySelector("h1").innerHTML="Player1 wins";
+document.querySelector("h1").innerHTML="Whichever Player you choose to be, Saumya Always Wins";
 else if(randomVar1<randomVar2)
 document.querySelector("h1").innerHTML="Player2 wins";
 else
