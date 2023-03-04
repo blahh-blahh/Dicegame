0556579395110d82ec5e91958f9e66b3365e7df2
blahh-blahh <85347713+blahh-blahh@users.noreply.github.com>
2023-03-04 18:29:40 +0530

    Update dicee.js

--- dicee.js
+++ dicee.js
@@ -1,6 +1,8 @@
 var state="initial";
 state=prompt("Select to be Player 1 or 2. Type 1 or 2");
-if(state=="initial"){}
+if(state=="initial"){
+ document.querySelector("h1").innerHTML="Saumya Always Wins";
+}
 else {
  document.querySelectorAll("p")[0].innerHTML="Player"+state;
   document.querySelectorAll("p")[1].innerHTML="Saumya";
@@ -10,9 +12,5 @@ var randomDiceImg1="images/dice"+randomVar1+".png";
 document.querySelector(".img1").setAttribute("src",randomDiceImg1);
 var randomDiceImg2="images/dice"+randomVar2+".png";
 document.querySelectorAll("img")[1].setAttribute("src",randomDiceImg2);
-if(randomVar1>randomVar2)
-document.querySelector("h1").innerHTML="Whichever Player you choose to be, Saumya Always Wins";
-else if(randomVar1<randomVar2)
-document.querySelector("h1").innerHTML="Player2 wins";
-else
-document.querySelector("h1").innerHTML="It's a tie.";}
+document.querySelector("h1").innerHTML="Saumya Always Wins";
+}
